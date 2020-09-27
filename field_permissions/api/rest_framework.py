3e7cf7c3f4cbc49bf62d83c46bf56d803e89a7b4
Lucas Souza <lucassouzaufpa@gmail.com>
2020-09-27 19:17:33 -0300

    Customiza compatibilidade com o rest_framework

--- field_permissions/api/rest_framework.py
+++ field_permissions/api/rest_framework.py
@@ -2,22 +2,50 @@
 API helpers for django-rest-framework.
 """
 
+from django.contrib.auth.models import AnonymousUser, Permission
+
 from rest_framework import serializers
+
 
 class FieldPermissionSerializerMixin:
     """
     ModelSerializer logic for marking fields as ``read_only=True`` when a user is found not to have
     change permissions.
     """
+
     def __init__(self, *args, **kwargs):
         super(FieldPermissionSerializerMixin, self).__init__(*args, **kwargs)
 
-        user = self.context['request'].user
+        request = self.context.get('request', None)
+        user = request.user if request else AnonymousUser()
         model = self.Meta.model
-        model_field_names = [f.name for f in model._meta.get_fields()]  # this might be too broad
+        model_name = model._meta.model_name
+        app_label = model._meta.app_label
+
+        # this might be too broad
+        model_field_names = [f.name for f in model._meta.get_fields()]
         for name in model_field_names:
-            if name in self.fields and not self.instance.has_field_perm(user, field=name):
+            in_fields = name in self.fields
+            has_perm = user.has_perm(
+                f'{app_label}.can_change_{model_name}_{name}'
+            )
+
+            if in_fields and not has_perm:
                 self.fields[name].read_only = True
+
+        for name in model_field_names:
+            in_fields = name in self.fields
+            permission_exists = Permission.objects.filter(
+                codename=f'can_view_{model_name}_{name}'
+            ).exists()
+            has_perm = user.has_perm(
+                f'{app_label}.can_view_{model_name}_{name}'
+            ) or user.has_perm(
+                f'{app_label}.can_change_{model_name}_{name}'
+            )
+
+            if in_fields and permission_exists and not has_perm:
+                self.fields.pop(name)
 
 
 class FieldPermissionSerializer(FieldPermissionSerializerMixin, serializers.ModelSerializer):
